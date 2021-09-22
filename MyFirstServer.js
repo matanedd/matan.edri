c74c845e085756c2bd8c7368043b54297c1108c4
matanedd <88576399+matanedd@users.noreply.github.com>
2021-09-22 17:12:34 +0300

    Add files via upload

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,10 +1,38 @@
+const sql = require("./db.js");
+    const mysql = require("mysql2");
+    const express = require("express");
+    const bodyParser = require("body-parser");
+    const app = express();
+
+    // parse requests of contenttype: application/json
+    app.use(bodyParser.json());
+    // parse requests of contenttype: application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true
+    }));
+
+    // simple route
+    app.get("/", (req, res) => {
+    res.json({ message: "Hello World !" });
+    });
+    // set port, listen for requests
+    app.listen(3000, () => {
+    console.log("Server is running on port 3000."
+    );
+    });
 
 
+    // Create a route for getting all customers
+app.get("/customers", function(req, res){
+    sql.query("SELECT * FROM customers", (err, mysqlres) => {
+    if (err) {
+    console.log("error: ", err);
+    res.status(400).send({message: "error in getting all customers: " + err});
+    return;
+    }
+    console.log("got all customers...");
+    res.send(mysqlres);
+    return;
+    });
+    });
 
-var http = require('http');
-
-http.createServer(function(req, res) {
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello World!');
-
-}).listen(8080);
+  
